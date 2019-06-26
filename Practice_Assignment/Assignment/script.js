5e1799d3397a605ab5e57f82dc397823281a0d61
Madhvi Kokil <madhvi.kokil@cuelogic.com>
2019-06-26 20:30:08 +0530

    26 June changes

--- Practice_Assignment/Assignment/script.js
+++ Practice_Assignment/Assignment/script.js
@@ -1,6 +1,6 @@
-  
-  
-  function register() {
+
+// Registration 
+function register() {
             var fname = document.signup.fname.value;
             var lname = document.signup.lname.value;
             var address = document.signup.address.value;
@@ -76,8 +76,6 @@
           if (username) 
           {
             alert('user is already present');
-          
-             
           } 
           
           else 
@@ -85,6 +83,7 @@
             parsedArray.push(obj);
             var parsedJSON = JSON.stringify(parsedArray);
             localStorage.setItem('users' ,parsedJSON);
+            window.open("login.html");
           }
         
           
@@ -96,78 +95,46 @@
           var myJSON = JSON.stringify(users);
            localStorage.setItem('users' ,myJSON);
         }
-        
-         
-
-          // localStorage.setItem('fname', fname);
-          // localStorage.setItem('lname', lname);
-          // localStorage.setItem('address', address);
-          // localStorage.setItem('pwd', pwd);
-          // localStorage.setItem('uname', uname);
-
-         console.log(users);
- 
-          window.open('login.html','_self');
+         window.open("login.html");
 }
-
-
-         
 
 // Login validation 
+function validation(){
 
-function validation()
-{
-   if(users.uname == uname2 && users.password == pwd2)
-      {
-        return true;
-      }
+  var unamesecond = document.login.unamesecond.value;
+  var pwdsecond = document.login.pwdsecond.value;
+    users = localStorage.getItem('users');
+    parsedArray = JSON.parse(users);
 
-      else if (usernameCheck) 
-      {
-          alert('Valid user');
-      } 
-    
-      else 
-      {
-          alert('invalid user');
+  var validUser = parsedArray.find(function(user2){
+    if(user2.uname === unamesecond && user2.pwd === pwdsecond){
+      return true;}
+    });
+
+    if(validUser){
+
+    if(sessionStorage){
+      // Store data
+      sessionStorage.setItem('unamesecond' ,unamesecond);
+      
+      // Retrieve data
+      alert("Hi, " +  sessionStorage.getItem('unamesecond'));
+      window.open("dashboard.html");
       }
-    
+     else{
+      alert("Sorry, your browser do not support session storage.");
+      }
 }
     
-
- /*var uname2 = document.login.uname2.value;
-  var pwd2 = document.login.pwd2.value;
-
-  if(uname2=="" ||pwd2==""){
-      alert("Fields should not be blank");
-      return false;
-  }
-  check();
- }
-
- // Checking function
-
-  function check(){   
-    
-    var storedName = localStorage.getItem(' uname');
-    var storedPwd = localStorage.getItem('pwd');
-
-    // entered data from the login-form
-    var uname2 = document.getElementById('uname2');
-    var pwd2 = document.getElementById('pwd2');
-
-    // check if stored data from register-form is equal to data from login form
-    if(uname2.value !== storedName || pwd2.value !== storedPwd) {
-        alert('ERROR');
-       window.open('index.html','_self');
-    }else {
-        alert('You are logged in.');
-        window.open('dashboard.html','_self');
+    else{
+      alert('invalid User');
+      window.open("login.html","_self");
+      }
     }
+
+  function login(){ 
+   window.open("login.html","_self");
   }
-*/
-//}
-  function login()
-  {
-    window.open('login.html','_self');
-  }
+
+  
+ 
